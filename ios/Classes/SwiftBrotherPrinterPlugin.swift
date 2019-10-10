d6d327d77a309be72862821a59b610f6707640aa
EzekR <renatus1812@163.com>
2019-10-10 13:45:45 +0800

    sd

--- ios/Classes/SwiftBrotherPrinterPlugin.swift
+++ ios/Classes/SwiftBrotherPrinterPlugin.swift
@@ -1,6 +1,6 @@
 import Flutter
 import UIKit
-import BRPtouchPrinterKitW
+import BRPtouchPrinterKit
 
 public class SwiftBrotherPrinterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
