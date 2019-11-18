3ddca0a45ddda3bfbb7f2272ff2b138332714b31
stef ren <renatus1812@163.com>
2019-11-18 14:19:49 +0800

    asdas
    asdasd

--- ios/Classes/BrotherPrinterPlugin.m
+++ ios/Classes/BrotherPrinterPlugin.m
@@ -1,5 +1,4 @@
 #import "BrotherPrinterPlugin.h"
-#import <brother_printer/brother_printer-Swift.h>
 
 @implementation BrotherPrinterPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
